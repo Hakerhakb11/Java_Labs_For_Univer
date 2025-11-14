6e9312c7c682cb87b8ef42a555e44b851c6c2bb1
Hakerhakb11 <igorshilov2007@gmail.com>
2025-11-14 20:15:26 +0600

    feat: Start do 4.2 lab work. ~style: litle style corrects

--- Lab_4/Task_2/Main.java
+++ Lab_4/Task_2/Main.java
@@ -2,7 +2,26 @@ package Lab_4.Task_2;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Lab 4.2");
+        System.out.println("Lab 4.2\n");
+
+        PriorityQueue pbeg = new PriorityQueueImpl();
+        
+        pbeg.enqueue(13);
+        pbeg.enqueue(11);
+        pbeg.enqueue(8);
+        pbeg.enqueue(4);
+        pbeg.enqueue(15);
+        pbeg.enqueue(11);
+        pbeg.enqueue(1);
+
+        pbeg.toString();
+
+
+        System.out.println("\nEND OF Lab 4.2");
     }
 }
-
+//       13
+//     /    \
+//    11     8
+//   / \    / \
+//  4  15  11  1
--- Lab_4/Task_2/PriorityQueueImpl.java
+++ Lab_4/Task_2/PriorityQueueImpl.java
@@ -1,9 +1,37 @@
 package Lab_4.Task_2;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class PriorityQueueImpl implements PriorityQueue<Integer> {
+    private Integer i = 0;
+    private List<Node> heap = new ArrayList<>();
+    public class Node {
+        private final Integer data;
+        private final Integer index;
+
+        Node(Integer data, Integer index) {
+            this.data = data;
+            this.index = index;
+
+
+        }
+        private int parent() {return (index - 1) / 2;}
+        private int left() {return index * 2 + 1;}
+        private int right() {return index * 2 + 2;}
+    }
+    
     @Override
     public void enqueue(Integer value) {
-	// TODO
+	    heap.add(new Node(value, i++));
+        System.out.println("\nTHIS IS I: " + i);
+            System.out.println("CHECK");
+            int thisData = heap.get(i - 1).data;
+            int parentData = heap.get(heap.get(i - 1).parent()).data;
+            if (parentData < thisData) {
+                System.out.println("NO EQUAL");
+                
+            }
     }
 
     @Override
@@ -16,4 +44,33 @@ public class PriorityQueueImpl implements PriorityQueue<Integer> {
     public void increment(long operation, Integer addition) {
 	// TODO
     }
-} 
+
+    // func to debug print Heap
+    @Override
+    public String toString() {
+        for (int j = 0; j < i; j++) {
+            System.out.print(heap.get(j).data + " " + heap.get(j).index);
+            System.out.println(" Parents: " + heap.get(heap.get(j).parent()).data);
+        }
+
+        //print heap interactive
+        System.out.print("      " + heap.get(0).data + "\r\n" + //
+        "    /    \\\r\n" + //
+        "   " + heap.get(1).data + "     " + heap.get(2).data + "\r\n" + //
+        "  / \\    / \\\r\n" + //
+        " " + heap.get(3).data + "  " + heap.get(4).data + "  " + heap.get(5).data + "  " + heap.get(6).data);
+        
+        return "This function print massiv of heap;";
+        //       13
+        //     /    \
+        //    11     8
+        //   / \    / \
+        //  4  15  11  1
+        
+        // System.out.print("      13\r\n" + //
+        //                 "    /    \\\r\n" + //
+        //                 "   11     8\r\n" + //
+        //                 "  / \\    / \\\r\n" + //
+        //                 " 4  15  11  1");
+    } 
+}
