ae06517592338a08bc51beee41cd4b8ee78c990f
Hakerhakb11 <igorshilov2007@gmail.com>
2025-11-22 15:10:37 +0600

    feat: replace code to func, and do little debug

--- Lab_6/Task_1/BinarySearch.java
+++ Lab_6/Task_1/BinarySearch.java
@@ -1,18 +1,19 @@
 package Lab_6.Task_1;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class BinarySearch {
-    public int searchNearValue(List<Integer> arr, List<Integer> request) {
+    public List<Integer> searchNearValue(List<Integer> arr, List<Integer> request) {
+        List<Integer> out = new ArrayList<>();
+
         for (int requestNum : request) {
             int right = arr.size() - 1;
             int left = 0;
             if (requestNum > arr.get(right)) {
-                // System.out.println(arr.get(right));
-                return arr.get(right);
+                out.add(arr.get(right));
             } else if (requestNum < arr.get(left)) {
-                // System.out.println(arr.get(left));
-                return arr.get(left);
+                out.add(arr.get(left));
             } else {
                 while (left <= right) {
                     int mid = left + (right - left) / 2;
@@ -25,13 +26,12 @@ public class BinarySearch {
                 int rightValue = left;
                 int leftValue = left - 1;
                 if (Math.abs(arr.get(leftValue) - requestNum) < Math.abs(arr.get(rightValue) - requestNum)) {
-                    // System.out.println(arr.get(leftValue));
-                    return arr.get(leftValue);
+                    out.add(arr.get(leftValue));
                 } else {
-                    // System.out.println(arr.get(rightValue));
-                    return arr.get(rightValue);
+                    out.add(arr.get(rightValue));
                 }
             }
         }
+        return out;
     }
 }
