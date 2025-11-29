2abc499c229216d1953be02937dae5dccf9525a5
Hakerhakb11 <igorshilov2007@gmail.com>
2025-11-29 12:36:49 +0600

    WIP: Half done 7.2 prog have iteration in arr, to find biggest arr in arr

--- Lab_7/Task_2/Main.java
+++ Lab_7/Task_2/Main.java
@@ -2,6 +2,7 @@ package Lab_7.Task_2;
 
 import java.util.List;
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,22 +11,38 @@ public class Main {
         List<Integer> arr = Arrays.asList(7, 1, 4, 3, 3, 5, 4, 8, 6, 9);
         for (int i : arr) {
             System.err.print(i + " ");
-        }System.err.println();
+        }System.err.println("\n");
 
-        List<Integer> bestArr = Arrays.asList(null, null, null, null, null, null, null, null, null, null);
-        List<Integer> tempArr = Arrays.asList(null, null, null, null, null, null, null, null, null, null);
+        List<Integer> bestArr = Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        List<Integer> tempArr = new ArrayList<>();
 
-        // List<Integer> bestArr = new ArrayList<>(10);
-
-
+        
         for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i) < arr.get(i + 1)) {
-                tempArr.set(i, arr.get(i));
-                tempArr.set(i + 1, arr.get(i + 1));
+            int k = 0;
+            int count = 1;
+            while (count > 0) {
+                int o = 0;
+                int checkNum = i;
+                count = 0;
+
+                tempArr.add(arr.get(checkNum));
+                for (int j = checkNum; j < arr.size(); j++) {
+                    if (arr.get(checkNum) < arr.get(j) && k - o++ < 1) {
+                        tempArr.add(arr.get(j));
+                        count++;
+                        checkNum = j;
+                    }
+                } 
+                k++;
+                
+                // 7 1 4 3 3 5 4 8 6 9
+                for (int j : tempArr) {
+                    System.err.print(j + " ");
+                }System.err.println();
+                System.err.println("\nend of cycle\n");
+                tempArr.clear();
             }
-            for (int j : tempArr) {
-            System.err.print(j + " ");
-            }System.err.println();
+            System.err.println("\nEND OF BIG cycle------------------------\n");
         }
         
         
