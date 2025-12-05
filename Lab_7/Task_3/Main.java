c0a55cc5d2a597d971f546da10a4c6a19fd9afdd
Hakerhakb11 <igorshilov2007@gmail.com>
2025-12-05 14:37:33 +0600

    WIP: Start doing 7.3 lab

--- Lab_7/Task_3/Main.java
+++ Lab_7/Task_3/Main.java
@@ -1,11 +1,39 @@
 package Lab_7.Task_3;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Start lab_7.3");
+        
+        String routeToDir = "Lab_7/Task_3/";
+
+        BufferedReader reader = new BufferedReader(new FileReader(routeToDir + "lis-input.txt"));
+        String line;
+        List<String> stringOut = new ArrayList<>();
+        while((line = reader.readLine()) != null) {
+            stringOut.add(line);
+        }
+
+        System.err.println(stringOut);
 
 
+        
+        
 
-        System.out.println("End lab_7.3");
+        String outPut = "CD 'test'";
+
+        System.err.println("\nres: " + outPut);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(routeToDir + "list-output.txt"));
+        writer.write(outPut);
+        reader.close();
+        writer.close();
+
+        System.out.println("\nEnd lab_7.3");
     }
 }
