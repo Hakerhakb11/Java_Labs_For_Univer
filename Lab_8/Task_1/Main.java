14a0dc588e74e2981869174890b9687569841213
Hakerhakb11 <igorshilov2007@gmail.com>
2025-12-06 15:10:29 +0600

    feat: Complete 8.1

--- Lab_8/Task_1/Main.java
+++ Lab_8/Task_1/Main.java
@@ -1,11 +1,30 @@
 package Lab_8.Task_1;
 
+import java.util.List;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start lab_8.1");
-
-
-
-        System.out.println("\nEnd lab_8.1");
+        int[] arr = {1, 4, 3, -6, 2, 6, -8, 2, 3, 5};
+        int x = 4;
+        int y = 4;
+        int sum = 0;
+        int sum2 = 0;
+        
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            prefix[i] += prefix[i - 1] + arr[i];
+        }
+        sum = prefix[y] - prefix[x - 1];
+        
+        // solution without prefix_sum algorithm
+        List<Integer> arr2 = Arrays.asList(1, 4, 3, -6, 2, 6, -8, 2, 3, 5);
+        for (int i = x; i <= y; i++) {
+            sum2 += arr2.get(i);
+        }
+        
+        System.err.println("\nResult: " + sum);
+        System.out.println("Must be: " + sum2);
     }
 }
