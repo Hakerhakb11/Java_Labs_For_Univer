5e9d2ecdd9d33c8698ce5785d76b6b3dc43d815c
Hakerhakb11 <igorshilov2007@gmail.com>
2025-12-09 18:40:59 +0600

    WIP: start do 9.1lab

---
+++ Lab_9/Task_1/BST.java
@@ -0,0 +1,16 @@
+package Lab_9.Task_1;
+
+
+public class BST {
+    private Node head = null;
+    class Node {
+        int value;
+        Node left;
+        Node right;
+    }
+
+    BST() {
+        System.out.println(2);
+        //TO DO;
+    }
+}
--- Lab_9/Task_1/Main.java
+++ Lab_9/Task_1/Main.java
@@ -1,9 +1,15 @@
 package Lab_9.Task_1;
 
+import java.util.List;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start lab_9.1");
 
+        List<Integer> arr = Arrays.asList(1,2,3,4,5,6,7,8);
+
+        System.out.println();
 
 
         System.out.println("\nEnd lab_9.1");
