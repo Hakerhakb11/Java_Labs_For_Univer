6a14035244202306c42687b60171b0261c0e0a01
Hakerhakb11 <igorshilov2007@gmail.com>
2025-12-18 15:46:53 +0600

    WIP: add func next() and prev()

--- Lab_9/Task_1/BSTree.java
+++ Lab_9/Task_1/BSTree.java
@@ -47,10 +47,10 @@ public class BSTree {
     public boolean has(int value) {
         Node current = head;
         while (current != null) {
-            if (current.data < value) {
-                current = current.right;
-            } else if (current.data > value) {
+            if (value < current.data) {
                 current = current.left;
+            } else if (value > current.data) {
+                current = current.right;
             } else {
                 return true;
             }
@@ -60,11 +60,13 @@ public class BSTree {
     
     public int next(int value) {
         Node current = head;
+        Node temp = null;
         
         while (current != null) {
             if (current.data < value) {
                 current = current.right;
             } else if (current.data > value) {
+                temp = current;
                 current = current.left;
             } else {
                 if (current.right != null) {
@@ -73,47 +75,59 @@ public class BSTree {
                         current = current.left;
                     }
                     return current.data;
+                } else {
+                    return temp.data;
                 }
             }
-            System.out.println("There!!!!" + current.data + " and: " + value);
         }
-        return 0;
+        return temp.data;
     }
+
+//        8
+//     /     \
+//    4        12
+//   / \      /  \
+//  2   6   10    14
+// / \ / \  / \   / \
+// 1 3 5 7 9  11 13 15
     
     public int prev(int value) {
         Node current = head;
+        Node temp = null;
         
-        while (current.data != value) {
+        while (current != null) {
             if (current.data < value) {
+                temp = current;
                 current = current.right;
             } else if (current.data > value) {
                 current = current.left;
             } else {
-                    current = current.right;
-
+                if (current.left != null) {
+                    current = current.left;
+                    while (current.right != null) {
+                        current = current.right;
+                    }
+                    return current.data;
+                } else {
+                    return temp.data;
+                }
             }
         }
-
-
-        return 0;
+        return temp.data;
     }
 
-    public void delete() {
-        //TO DO
+    public void delete(int value) {
+        //To do
     }
 
+    // print tree interactive || Inputable tree.length must be = 15 
     public void printTree() {
-        // print tree interactive || Inputable tree.length must be = 16 
-        // if (head.length <= 16) {
-            System.out.print("\n       " + head.data + "\r\n" + //
-            "    /     \\\r\n" + //
-            "   " + head.left.data + "        " + head.right.data + "\r\n" + //
-            "  / \\      /  \\\r\n" + //
-            " " + head.left.left.data + "   " + head.left.right.data + "   " + head.right.left.data + "    " + head.right.right.data + "\r\n" + //
-                    "/ \\ / \\  / \\   / \\\r\n" + //"");
-            "" + head.left.left.left.data + " " + head.left.left.right.data + " " + head.left.right.left.data + " " + head.left.right.right.data + " " + head.right.left.left.data + "  " + head.right.left.right.data + " " + head.right.right.left.data + "  " + head.right.right.right.data + "\n\n");
-        // } else {
-            System.out.println("Интерактивный вывод доступен только если arr.length <= 8");
-        // }
+        System.out.print("\n       " + head.data + "\r\n" + //
+        "    /     \\\r\n" + //
+        "   " + head.left.data + "        " + head.right.data + "\r\n" + //
+        "  / \\      /  \\\r\n" + //
+        " " + head.left.left.data + "   " + head.left.right.data + "   " + head.right.left.data + "    " + head.right.right.data + "\r\n" + //
+                "/ \\ / \\  / \\   / \\\r\n" + //"");
+        "" + head.left.left.left.data + " " + head.left.left.right.data + " " + head.left.right.left.data + " " + head.left.right.right.data + " " + head.right.left.left.data + "  " + head.right.left.right.data + " " + head.right.right.left.data + "  " + head.right.right.right.data + "\n\n");
     }
 }
--- Lab_9/Task_1/Main.java
+++ Lab_9/Task_1/Main.java
@@ -5,15 +5,7 @@ public class Main {
         System.out.println("Start lab_9.1");
 
         BSTree bstree = new BSTree();
-
-//        8
-//     /     \
-//    4        12
-//   / \      /  \
-//  2   6   10    14
-// / \ / \  / \   / \
-// 1 3 5 7 9  11 13 15
-
+        
         bstree.insert(8);
         bstree.insert(4);
         bstree.insert(2);
@@ -29,11 +21,71 @@ public class Main {
         bstree.insert(14);
         bstree.insert(13);
         bstree.insert(15);
+        
+        System.out.println(bstree.has(2));
+        
+        System.out.println(bstree.next(2));
+        
+        System.out.println(bstree.prev(5));
 
-        System.out.println(bstree.next(16));
+        // bstree.delete(6);
         
         bstree.printTree();
-
+        
         System.out.println("\nEnd lab_9.1");
     }
 }
+
+// Big befutifule tree 1-15
+//        8
+//     /     \
+//    4        12
+//   / \      /  \
+//  2   6   10    14
+// / \ / \  / \   / \
+// 1 3 5 7 9  11 13 15
+
+// bstree.insert(8);
+// bstree.insert(4);
+// bstree.insert(2);
+// bstree.insert(1);
+// bstree.insert(3);
+// bstree.insert(6);
+// bstree.insert(5);
+// bstree.insert(7);
+// bstree.insert(12);
+// bstree.insert(10);
+// bstree.insert(11);
+// bstree.insert(9);
+// bstree.insert(14);
+// bstree.insert(13);
+// bstree.insert(15);
+
+// Second tree, without 2 and 6.
+//        5
+//     /     \
+//    3        8
+//   / \      /  \
+//  1   4   7    9
+
+// bstree.insert(5);
+// bstree.insert(3);
+// bstree.insert(8);
+// bstree.insert(1);
+// bstree.insert(4);
+// bstree.insert(7);
+// bstree.insert(9);
+
+// Test For Univer Lab.
+// bstree.insert(5);
+// bstree.insert(7);
+// bstree.insert(3);
+// bstree.insert(4);
+// System.out.println(bstree.has(3));
+// System.out.println(bstree.has(8));
+// bstree.insert(8);
+// System.out.println(bstree.has(8));
+// // System.out.println(bstree.delete(3));
+// System.out.println(bstree.has(4));
+// System.out.println(bstree.next(7));
+// System.out.println(bstree.prev(5));
