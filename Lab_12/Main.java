2b16f94c5a70c79e6c0cb728a97d4173a980cfdb
Hakerhakb11 <igorshilov2007@gmail.com>
2026-05-15 20:24:31 +0600

    feat: Finally i did it. LAB_12 is DONE. My colculator for your attention, please make it subscribe to my GitHub

---
+++ Lab_12/Main.java
@@ -0,0 +1,18 @@
+package Lab_12; // This is a Name of Direction where is this programm is located. In java you need to write like that.
+// empty place. This place doesn't effect to code Anyway don't worry
+public class Main { // This is a Main class, that have in every program, if it Entry point of programm. Unlike Python, you Must use Class in every Program that you write
+    public static void main(String[] args) {
+    System.out.println("Start lab 12"); // this is a Input_output part of programm
+// empty place. This place doesn't effect to code Anyway don't worry
+    System.out.println("Welcome to COLCULATOR"); // this is hello_messages()
+    int input1 = 34; // First num what user input in this Programm
+    System.out.println("First num:" + input1); // this is a Input_output part of programm
+    int input2 = 33; // Second num what user input in this Programm
+    System.out.println("Second num:" + input2); // this is a Input_output part of programm
+// empty place. This place doesn't effect to code Anyway don't worry
+    int output = input1 + input2; // This is a result of Colculator Algorithm. What used to resolve hard mathemathical questions.
+    System.out.println("result: " + output); // this is a Input_output part of programm
+// empty place. This place doesn't effect to code Anyway don't worry
+    System.out.println("End lab 12"); // this is a Input_output part of programm
+    } // this is a Closing Parenthesis os public static void main Function. That function usually have in every programm in Java
+} // This is a Endest Closing Parenthesis. It is Necessary to been that Parenthesis here. Becose Java is not a Python Language, it's not so eazy.
